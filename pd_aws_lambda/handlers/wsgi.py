13bff48b6ee508b7fba6372fb62f3f628b2a60d2
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2021-03-02 23:15:00 +0100

    Fix cookie value escaping

--- pd_aws_lambda/handlers/wsgi.py
+++ pd_aws_lambda/handlers/wsgi.py
@@ -1,4 +1,5 @@
 """This module converts an AWS API Gateway proxied request to a WSGI request."""
+from codecs import decode, encode
 
 from apig_wsgi import (
     DEFAULT_NON_BINARY_CONTENT_TYPE_PREFIXES,
@@ -10,6 +11,10 @@ from apig_wsgi import (
 def handler(app, event, context):
     """Process an HTTP Lambda request with `apig_wsgi`."""
     environ = get_environ_v2(event, context, binary_support=True)
+    # https://stackoverflow.com/questions/1885181/how-to-un-escape-a-backslash-escaped-string/57192592#57192592
+    environ["HTTP_COOKIE"] = decode(
+        encode(environ["HTTP_COOKIE"], "latin-1", "backslashreplace"), "unicode-escape"
+    )
     response = V2Response(
         binary_support=True,
         non_binary_content_type_prefixes=DEFAULT_NON_BINARY_CONTENT_TYPE_PREFIXES,
