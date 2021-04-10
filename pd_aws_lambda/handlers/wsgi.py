79d471a681c02df49fc23e3bd1a4f456352393ff
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2021-04-10 17:56:43 +0200

    Convert PATH_INFO to iso-8859-1

--- pd_aws_lambda/handlers/wsgi.py
+++ pd_aws_lambda/handlers/wsgi.py
@@ -11,10 +11,17 @@ from apig_wsgi import (
 def handler(app, event, context):
     """Process an HTTP Lambda request with `apig_wsgi`."""
     environ = get_environ_v2(event, context, binary_support=True)
+
     # https://stackoverflow.com/questions/1885181/how-to-un-escape-a-backslash-escaped-string/57192592#57192592
     environ["HTTP_COOKIE"] = decode(
         encode(environ["HTTP_COOKIE"], "latin-1", "backslashreplace"), "unicode-escape"
     )
+    # WSGI should be iso-8859-1 encoded, but lambda provides utf-8 strings, and
+    # `apig_wsgi` is not taking that into account.
+    environ["PATH_INFO"] = (
+        environ["PATH_INFO"].encode("utf-8", "replace").decode("iso-8859-1", "replace")
+    )
+
     response = V2Response(
         binary_support=True,
         non_binary_content_type_prefixes=DEFAULT_NON_BINARY_CONTENT_TYPE_PREFIXES,
